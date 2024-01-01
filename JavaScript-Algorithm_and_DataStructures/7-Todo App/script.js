ffd88b3b21d7dae17e802374646aa631c4ef1ecb
Prince NZAMUWE(Fudji Bokande) <134228300+princenzmw@users.noreply.github.com>
2024-01-01 23:28:59 +0200

    Step 25 - Inside the reset function, set each value of titleInput, dateInput, descriptionInput to an empty string.
    Also, use classList to toggle the class hidden on the taskForm and set currentTask to an empty object. That's because at this point, currentTask will be filled with the task the user might have added.

--- JavaScript-Algorithm_and_DataStructures/7-Todo App/script.js
+++ JavaScript-Algorithm_and_DataStructures/7-Todo App/script.js
@@ -13,7 +13,14 @@ const descriptionInput = document.getElementById('description-input');
 const taskData = [];
 let currentTask = {};
 
-const reset = () => {};
+const reset = () => {
+    titleInput.value = '';
+    dateInput.value = '';
+    descriptionInput.value = '';
+
+    taskForm.classList.toggle('hidden');
+    currentTask = {};
+};
 
 openTaskFormBtn.addEventListener('click', () => taskForm.classList.toggle('hidden'));
 closeTaskFormBtn.addEventListener('click', () => confirmCloseDialog.showModal());
