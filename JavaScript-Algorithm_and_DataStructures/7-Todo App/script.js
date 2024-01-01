a27429d649a3b7c0e7129a4ab7f9bb6810128d3a
Prince NZAMUWE(Fudji Bokande) <134228300+princenzmw@users.noreply.github.com>
2024-01-01 23:11:47 +0200

    Step 22 - To allow for task management, you need to include both a delete and an edit button for each task.
    Create two button elements with the type attribute set to button and the class attribute set to btn. Set the text of the first button to Edit and the text of the second button to Delete.

--- JavaScript-Algorithm_and_DataStructures/7-Todo App/script.js
+++ JavaScript-Algorithm_and_DataStructures/7-Todo App/script.js
@@ -40,5 +40,7 @@ taskForm.addEventListener('submit', (e) => {
     <p><strong>Title: </strong>${title}</p>
     <p><strong>Date: </strong>${date}</p>
     <p><strong>Description: </strong>${description}</p>
+    <button type="button" class="btn">Edit</button>
+    <button type="button" class="btn">Delete</button>
     `);
 });
