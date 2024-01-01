ac97227d737ec70026110c55b3ab1086f8f94b78
Prince NZAMUWE(Fudji Bokande) <134228300+princenzmw@users.noreply.github.com>
2024-01-02 00:06:34 +0200

    Step 34 - There's a problem. If you add a task, and then add another, the previous task gets duplicated. This means you need to clear out the existing contents of tasksContainer before adding a new task.
    Set the innerHTML of tasksContainer back to an empty string.

--- JavaScript-Algorithm_and_DataStructures/7-Todo App/script.js
+++ JavaScript-Algorithm_and_DataStructures/7-Todo App/script.js
@@ -31,6 +31,7 @@ const addOrUpdateTask = () => {
 }
 
 const updateTaskContainer = () => {
+    tasksContainer.innerHTML = '';
     taskData.forEach(({ id, title, date, description }) =>
         (tasksContainer.innerHTML += `
             <div class="task" id="${id}">
