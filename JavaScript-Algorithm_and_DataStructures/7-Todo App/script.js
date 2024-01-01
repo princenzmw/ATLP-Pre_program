4a14ea9fdd8e1855ffb6aabe6f0f1370f22d6b5f
Prince NZAMUWE(Fudji Bokande) <134228300+princenzmw@users.noreply.github.com>
2024-01-02 00:55:42 +0200

    Step 43 - Set the innerText of the addOrUpdateTaskBtn button to Update Task and its aria-label to Update Task as well.

--- JavaScript-Algorithm_and_DataStructures/7-Todo App/script.js
+++ JavaScript-Algorithm_and_DataStructures/7-Todo App/script.js
@@ -64,6 +64,9 @@ const editTask = (buttonEl) => {
     titleInput.value = currentTask.title;
     dateInput.value = currentTask.date;
     descriptionInput.value = currentTask.description;
+
+    addOrUpdateTaskBtn.innerText = 'Update Task';
+    addOrUpdateTaskBtn.ariaLabel= 'Update Task';
 };
 
 const reset = () => {
