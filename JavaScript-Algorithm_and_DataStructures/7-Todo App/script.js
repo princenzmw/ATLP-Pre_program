1a50c51b6fd5b554fb7330ed051d82de29e3e25d
Prince NZAMUWE(Fudji Bokande) <134228300+princenzmw@users.noreply.github.com>
2024-01-01 23:01:19 +0200

    Step 18 - Create a div element with the class of task. Utilize template strings to set the id attribute of the div to the id you destructured from the task data.

--- JavaScript-Algorithm_and_DataStructures/7-Todo App/script.js
+++ JavaScript-Algorithm_and_DataStructures/7-Todo App/script.js
@@ -35,5 +35,7 @@ taskForm.addEventListener('submit', (e) => {
     if (dataArrIndex === -1) {
         taskData.unshift(taskObj);
     }
-    taskData.forEach(({ id, title, date, description }) => tasksContainer.innerHTML += ``);
+    taskData.forEach(({ id, title, date, description }) => tasksContainer.innerHTML += `
+    <div class="task" id="${id}"></div>
+    `);
 });
