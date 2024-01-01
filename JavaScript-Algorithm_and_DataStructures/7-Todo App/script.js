0b7c037cdb288e404568e7fdc3b9c173d28fe8d1
Prince NZAMUWE(Fudji Bokande) <134228300+princenzmw@users.noreply.github.com>
2024-01-01 23:06:15 +0200

    Step 20 - Similarly to the previous step, create another p element, and interpolate the date you destructured as the text content. Inside this paragraph, create a strong element with the text Date:.

--- JavaScript-Algorithm_and_DataStructures/7-Todo App/script.js
+++ JavaScript-Algorithm_and_DataStructures/7-Todo App/script.js
@@ -38,5 +38,6 @@ taskForm.addEventListener('submit', (e) => {
     taskData.forEach(({ id, title, date, description }) => tasksContainer.innerHTML += `
     <div class="task" id="${id}"></div>
     <p><strong>Title: </strong>${title}</p>
+    <p><strong>Date: </strong>${date}</p>
     `);
 });
