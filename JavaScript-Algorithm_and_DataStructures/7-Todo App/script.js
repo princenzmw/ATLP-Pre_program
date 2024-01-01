14c50b2c811e7b2ed72c4705ec37dbbe49455650
Prince NZAMUWE(Fudji Bokande) <134228300+princenzmw@users.noreply.github.com>
2024-01-02 01:09:25 +0200

    Step 47 - Now add formInputValuesUpdated as the second mandatory condition in the if statement using the AND operator.
    This way, the Cancel and Discard buttons in the modal won't be displayed to the user if they haven't made any changes to the input fields while attempting to edit a task.

--- JavaScript-Algorithm_and_DataStructures/7-Todo App/script.js
+++ JavaScript-Algorithm_and_DataStructures/7-Todo App/script.js
@@ -90,7 +90,7 @@ closeTaskFormBtn.addEventListener('click', () => {
 
     const formInputValuesUpdated = titleInput.value !== currentTask.title || dateInput.value !== currentTask.date || descriptionInput.value !== currentTask.description;
 
-    if (formInputsContainValues) {
+    if (formInputsContainValues && formInputValuesUpdated) {
         confirmCloseDialog.showModal();
     }
     else {
