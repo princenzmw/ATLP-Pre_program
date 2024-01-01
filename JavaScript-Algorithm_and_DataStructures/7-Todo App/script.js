551f1471610858992423d8ca64d9dadea9b1692a
Prince NZAMUWE(Fudji Bokande) <134228300+princenzmw@users.noreply.github.com>
2024-01-02 00:47:24 +0200

    Step 41 - Use square bracket notation to retrieve the task to be edited from the taskData array using the dataArrIndex. Then, assign it to the currentTask object to keep track of it.

--- JavaScript-Algorithm_and_DataStructures/7-Todo App/script.js
+++ JavaScript-Algorithm_and_DataStructures/7-Todo App/script.js
@@ -58,6 +58,8 @@ const editTask = (buttonEl) => {
     const dataArrIndex = taskData.findIndex(
         (item) => item.id === buttonEl.parentElement.id
     );
+
+    currentTask = taskData[dataArrIndex];
 };
 
 const reset = () => {
