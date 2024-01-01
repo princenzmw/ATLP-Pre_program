8d4bf4965aa415a4a6774819cd2372ba39e5f1f8
Prince NZAMUWE(Fudji Bokande) <134228300+princenzmw@users.noreply.github.com>
2024-01-02 00:13:49 +0200

    Step 35 - To enable editing and deleting for each task, add an onclick attribute to both buttons. Set the value of the onclick attribute to editTask(this) for the Edit button and deleteTask(this) for the Delete button. The editTask(this) function will handle editing, while the deleteTask(this) function will handle deletion.
    this is a keyword that refers to the current context. In this case, this points to the element that triggers the event – the buttons.

--- JavaScript-Algorithm_and_DataStructures/7-Todo App/script.js
+++ JavaScript-Algorithm_and_DataStructures/7-Todo App/script.js
@@ -32,14 +32,15 @@ const addOrUpdateTask = () => {
 
 const updateTaskContainer = () => {
     tasksContainer.innerHTML = '';
+
     taskData.forEach(({ id, title, date, description }) =>
         (tasksContainer.innerHTML += `
             <div class="task" id="${id}">
                 <p><strong>Title: </strong>${title}</p>
                 <p><strong>Date: </strong>${date}</p>
                 <p><strong>Description: </strong>${description}</p>
-                <button type="button" class="btn">Edit</button>
-                <button type="button" class="btn">Delete</button>
+                <button onclick="editTask(this)" type="button" class="btn">Edit</button>
+                <button onclick="deleteTask(this)" type="button" class="btn">Delete</button>
             </div>
         `)
     );
