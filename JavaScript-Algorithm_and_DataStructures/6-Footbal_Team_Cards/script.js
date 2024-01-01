09d7eb5827efcb030009c710b17234dcf8c1ae94
Prince NZAMUWE(Fudji Bokande) <134228300+princenzmw@users.noreply.github.com>
2024-01-01 17:40:21 +0200

    Step 26 - The next step is to create a new array that will be responsible for adding the player card information to the page.
    Inside the setPlayerCards function, start by adding the map method to arr that will take in an empty callback function. Then, use the addition assignment += operator to assign the new array to playerCards.innerHTML.

--- JavaScript-Algorithm_and_DataStructures/6-Footbal_Team_Cards/script.js
+++ JavaScript-Algorithm_and_DataStructures/6-Footbal_Team_Cards/script.js
@@ -181,3 +181,4 @@ teamName.textContent = team;
 worldCupYear.textContent = year;
 headCoach.textContent = coachName;
 const setPlayerCards = (arr = players) => {};
+playerCards.innerHTML += arr.map(() => {});
