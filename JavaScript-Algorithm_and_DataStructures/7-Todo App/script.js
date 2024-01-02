4d3cae8c17569c16c986ae31c2bf8db9581dfdf2
Prince NZAMUWE(Fudji Bokande) <134228300+princenzmw@users.noreply.github.com>
2024-01-02 09:54:51 +0200

    Step 53 - Using localStorage.clear() won't just delete a single item from local storage but will remove all items.
    Remove localStorage.removeItem() and use localStorage.clear() instead. You don't need to pass in anything. You should also see null in the console.

--- JavaScript-Algorithm_and_DataStructures/7-Todo App/script.js
+++ JavaScript-Algorithm_and_DataStructures/7-Todo App/script.js
@@ -119,8 +119,7 @@ const myTaskArr = [
 
 localStorage.setItem("data", JSON.stringify(myTaskArr));
 
-localStorage.removeItem("data");
-console.log();
+localStorage.clear();
 
 const getTaskArr = localStorage.getItem("data");
 console.log(getTaskArr);
